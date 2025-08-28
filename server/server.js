c495e54b325060c76b3f14dba0a1fc48a5896d01
Chandu <159621494+shashanthroyal@users.noreply.github.com>
2025-08-28 09:58:13 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -8,7 +8,10 @@ ConnectDB()
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+  origin: "https://task-manager-d8yb.vercel.app",
+  credentials: true
+})))
 app.use(express.json())
 
 app.use('/api/tasks', require('./routes/taskRoutes'))
